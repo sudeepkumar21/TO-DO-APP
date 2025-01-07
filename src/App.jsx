b773cb01414fb55faa0126b77831bbeb7953e0c3
Sudeep Kumar <sudeepkumar3s3@gmail.com>
2025-01-07 21:33:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToDoList from './ToDoList'
+import "./app.css"
+
+function App() {
+  return (
+    <div>
+      <ToDoList/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/ToDoList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
+// uuidv4(); 
+
+function ToDoList() {
+    const [tasks,setTasks]=useState([{todo:"sample task",id:uuidv4()}])
+    const [newTask,setNewTask]=useState("");
+
+    uuidv4(); 
+
+    //add input value
+    const handleInputChange=(event)=>
+    {
+        setNewTask(event.target.value)
+    }
+
+    const addNewTask=()=>{
+        setTasks([...tasks, { todo: newTask, id: uuidv4() }]);
+        setNewTask("")
+        
+    }
+   
+  return (
+    <div>
+      <input placeholder="Add a task" value={newTask} onChange={handleInputChange}></input>
+      <br></br>
+      <button onClick={addNewTask}>Submit</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      <hr></hr>
+      <h4>Tasks To Do</h4>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.todo}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
+}
+
+export default ToDoList
