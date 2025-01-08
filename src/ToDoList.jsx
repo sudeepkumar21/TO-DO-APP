61d61b9f1df6f632d9637f88f6491bfbb668fd52
Sudeep Kumar <sudeepkumar3s3@gmail.com>
2025-01-08 12:04:54 +0530

    second commit

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -16,28 +16,37 @@ function ToDoList() {
     }
 
     const addNewTask=()=>{
-        setTasks([...tasks, { todo: newTask, id: uuidv4() }]);
+        setTasks((PrevTask)=>{
+          return [...PrevTask, { todo: newTask, id: uuidv4() }];
+
+        });
         setNewTask("")
         
     }
    
   return (
     <div>
-      <input placeholder="Add a task" value={newTask} onChange={handleInputChange}></input>
+      <input
+        placeholder="Add a task"
+        value={newTask}
+        onChange={handleInputChange}
+      ></input>
       <br></br>
       <button onClick={addNewTask}>Submit</button>
       <br></br>
       <br></br>
       <br></br>
-      
+
       <hr></hr>
       <h4>Tasks To Do</h4>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>{task.todo}</li>
+          <li key={task.id}>
+            <span>{task.todo}</span>
+            <button>Delete</button>
+          </li>
         ))}
       </ul>
-
     </div>
   );
 }
